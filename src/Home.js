9151f32ff982bd2c7b78cd9632a7bfc179283f86
jtskidmore <johntoddskidmore@gmail.com>
2020-12-14 14:43:17 -0700

    formatted home page, added drop shadows to images

--- src/Home.js
+++ src/Home.js
@@ -3,35 +3,44 @@ import './App.css';
 import { Grid, Cell } from 'react-mdl';
 
 export default class Home extends React.Component {
-    render () {
+    render() {
         return (
-            <div style={{width: "100%", margin: 'auto'}}>
+            <div style={{ width: "100%", margin: 'auto' }}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        
-                        <div className="banner-text">
-                            <h1>Web Developer</h1>
-                            <hr/>
-                            <p>
-                               Javascript | React | Ruby | Ruby on Rails | HTML | CSS 
+                        <div className="left-container">
+                            <div className="banner-text">
+                                <img id="homepage-logo" src="jt_logo.png" alt="logo" />
+                                <h1>Web Developer</h1>
+                                <hr />
+                                <p>
+                                    Javascript | React | Ruby | Ruby on Rails | HTML | CSS
                             </p>
-                            <div className="social-links">
+                                <div className="social-links">
 
-                                {/* LinkedIn */}
-                                <a href="https://www.linkedin.com/in/johntoddskidmore/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
-                                </a>
+                                    {/* LinkedIn */}
+                                    <a className="hvr-float-shadow" href="https://www.linkedin.com/in/johntoddskidmore/" rel="noopener noreferrer" target="_blank">
+                                        <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                    </a>
 
-                                {/* Github */}
-                                <a href="https://www.linkedin.com/in/johntoddskidmore/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-github-square" aria-hidden="true"/>
-                                </a>
+                                    {/* Github */}
+                                    <a className="hvr-float-shadow" href="https://www.linkedin.com/in/johntoddskidmore/" rel="noopener noreferrer" target="_blank">
+                                        <i className="fa fa-github-square" aria-hidden="true" />
+                                    </a>
 
-                                {/* Medium */}
-                                <a href="https://www.linkedin.com/in/johntoddskidmore/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-medium" aria-hidden="true"/>
-                                </a>
+                                    {/* Medium */}
+                                    <a className="hvr-float-shadow" href="https://www.linkedin.com/in/johntoddskidmore/" rel="noopener noreferrer" target="_blank">
+                                        <i className="fa fa-medium" aria-hidden="true" />
+                                    </a>
 
+                                </div>
+                            </div>
+                        </div>
+                    </Cell>
+                    <Cell>
+                        <div className="graphic">
+                            <div className="right-container">
+                                <img id="homepage-graphic" src="graphic2.png" alt="logo" />
                             </div>
                         </div>
                     </Cell>
--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './App.css';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 
 export default class Projects extends React.Component {
-    render () {
+    render() {
         return (
-            <div className="projects-container">
-                <h1>This is the projects page</h1>
-            </div>
+            <CarouselProvider
+                naturalSlideWidth={100}
+                naturalSlideHeight={125}
+                totalSlides={3}
+            >
+                <Slider>
+                    <Slide index={0}>I am the first Slide.</Slide>
+                    <Slide index={1}>I am the second Slide.</Slide>
+                    <Slide index={2}>I am the third Slide.</Slide>
+                </Slider>
+                <ButtonBack>Back</ButtonBack>
+                <ButtonNext>Next</ButtonNext>
+            </CarouselProvider>
         )
     }
 }
